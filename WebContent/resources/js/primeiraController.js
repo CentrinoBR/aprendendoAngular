e90b620abfa5823b39f01216a08313a5ef462c14
Railson Rames <railson.rames@gmail.com>
2017-09-24 16:40:01 -0300

    Finalizada a primeira parte do curso de Angular
    Conceitos iniciais e algumas propriedades da ferramenta.
    O curso é hospedado na Udemy, do site javaavancado.com

---
+++ WebContent/resources/js/primeiraController.js
@@ -0,0 +1,14 @@
+var app = angular.module('Teste', []);
+
+app.controller('primeiraController', ['$scope', function($scope) {
+	$scope.prima = {meuNome : 'Railson Ramés'};
+	
+	$scope.contador = 0;
+	
+	$scope.addAoContador = function() {
+		$scope.contador++;	
+	};
+	
+	$scope.pessoas = ['Rayanne', 'Railson', 'Rayanninha, a.k.a. Clarice', 'Robertinho a.k.a. Henrique', 'Que sucesso juventude'];
+	
+}]);
